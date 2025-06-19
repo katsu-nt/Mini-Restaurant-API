6bc2d9b9e436a7b8a9382a1c73248c876112b234
ben <benbilldev@gmail.comm>
2025-06-19 17:12:05 +0700

    chore: set up project follow clean architecture

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDBContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+public class RestaurantsDBContext(DbContextOptions<RestaurantsDBContext> options) : DbContext(options)
+{
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>().HasMany(r => r.Dishes).WithOne().HasForeignKey(d => d.RestaurantId);
+    }
+}
