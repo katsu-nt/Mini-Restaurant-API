d38fa873b2c3d9d9c83fecbe368e40839ac5ff88
ben <benbilldev@gmail.comm>
2025-06-23 09:45:35 +0700

    feat(Authorization): unassigning user role

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands;
 using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnassignUserRole;
 using Restaurants.Domain.Contants;
 
 namespace Restaurants.API.Controllers;
@@ -23,6 +24,13 @@ public class IdentityController(IMediator mediator) : ControllerBase
     [HttpPost("userRole")]
     [Authorize(Roles = UserRoles.Admin)]
     public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
+    [HttpDelete("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
     {
         await mediator.Send(command);
         return NoContent();
